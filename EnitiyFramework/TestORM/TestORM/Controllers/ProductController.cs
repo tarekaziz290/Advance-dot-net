365de90ea97c97d0a80c8c62eb2cc3cf1dbf066e
tarekaziz290 <62283332+tarekaziz290@users.noreply.github.com>
2021-10-10 12:25:05 +0600

    ORM Testing

---
+++ EnitiyFramework/TestORM/TestORM/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestORM.Models;
+
+namespace TestORM.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var db = new testEntities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+       public ActionResult Create(Product p)
+        {
+            var d = new testEntities();
+            d.Products.Add(p);
+            d.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
