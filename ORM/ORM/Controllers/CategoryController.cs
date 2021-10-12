05855ee1debf5525670ed51050fd2b6e5c221454
tarekaziz290 <62283332+tarekaziz290@users.noreply.github.com>
2021-10-12 21:45:01 +0600

    Relationship with two tables

---
+++ ORM/ORM/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using ORM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORM.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var db = new testEntities();
+            var data = db.Categories.ToList();
+            return View(data);
+           
+        }
+        public ActionResult Details(int Id)
+        {
+            var db = new testEntities();
+            var data = (from c in db.Categories where c.Id == Id select c).FirstOrDefault();
+            return View(data);
+
+        }
+    }
+}
---
+++ ORM/ORM/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ORM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ORM.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var db = new testEntities();
+            var data = db.Products.ToList();
+            return View(data );
+        }
+    }
+}
