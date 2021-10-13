31e5582fa884e5314f4294cae1e60b6248230ffe
tarekaziz290 <62283332+tarekaziz290@users.noreply.github.com>
2021-10-13 10:50:10 +0600

    All crud done

--- ORM/ORM/Controllers/ProductController.cs
+++ ORM/ORM/Controllers/ProductController.cs
@@ -16,5 +16,52 @@ namespace ORM.Controllers
             var data = db.Products.ToList();
             return View(data );
         }
+
+        public ActionResult Details(int Id)
+        {
+            var db = new testEntities();
+            var data = (from c in db.Products where c.Id == Id select c).FirstOrDefault();
+            return View(data);
+
+        }
+        public ActionResult Edit(int Id)
+        {
+            var db = new testEntities();
+            var data = (from c in db.Products where c.Id == Id select c).FirstOrDefault();
+            return View(data);
+
+        }
+        [HttpPost]
+        public ActionResult Edit(Product P)
+        {
+            var db = new testEntities();
+            var data = (from c in db.Products where c.Id == P.Id select c).FirstOrDefault();
+            db.Entry(data).CurrentValues.SetValues(P);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+           
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            var db = new testEntities();
+            db.Products.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Product P)
+        {
+            var db = new testEntities();
+            var data = (from c in db.Products where c.Id == P.Id select c).FirstOrDefault();
+            db.Products.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
